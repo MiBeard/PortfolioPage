d04d0b91a3d0044937f82616484f2210d78723c0
Your Name Here <ewise@test.comclear>
2018-08-28 16:52:13 -0400

    resume

--- home.js
+++ home.js
@@ -2,16 +2,32 @@ $(document).ready(function () {
 
     $("#resume").hide();
 
-    $("button").click(function () {
+    $(".resumeButton").click(function () {
+        $(".aboutMeButton").toggle();
         $("#welcomePage").slideToggle("slow","swing");
         $("#pageHeader").slideToggle("slow","swing");
     });
 
-    $("button").click(function () {
+    $(".resumeButton").click(function () {
         $(this).text(function (i, v) {
             return v === 'Home' ? 'Resume' : 'Home'
         })
         $("#resume").toggle();
+    });
+
+    $("#aboutMe").hide();
+
+    $(".aboutMeButton").click(function () {
+        $(".resumeButton").toggle();
+        $("#welcomePage").slideToggle("slow","swing");
+        $("#pageHeader").slideToggle("slow","swing");
+    });
+
+    $(".aboutMeButton").click(function () {
+        $(this).text(function (i, v) {
+            return v === 'Home' ? 'About Me' : 'Home'
+        })
+        $("#aboutMe").toggle();
     });
 
 });
